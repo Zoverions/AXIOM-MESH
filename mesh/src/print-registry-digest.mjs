4674a98a67550c845b41ecee7e7f1cffa394a8bd
Zoverions <zoverions@gmail.com>
2026-08-10 00:08:28 -0400

    chore: remove temporary registry digest probe

--- mesh/src/print-registry-digest.mjs
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'node:fs/promises';
-import { digestObject } from './lib/canonical.mjs';
-
-const registry = JSON.parse(await readFile(new URL('../config/capabilities.json', import.meta.url), 'utf8'));
-console.log(JSON.stringify({ probe_registry_digest: digestObject(registry) }));
